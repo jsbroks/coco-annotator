2b75f0a4f92f92d27e8865292b8fc955a02a2a25
Justin Brooks <jsbroks@gmail.com>
2018-12-19 10:30:17 -0500

    Navbar testing

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -6,10 +6,6 @@
       <span class="subscript">{{ tag }}</span>
     </a>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon" />
-    </button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item" :class="{ active: $route.name === 'datasets' }">
