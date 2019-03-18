ebb30b4d559a2246822103032cb561c5219a955f
Justin Brooks <jsbroks@gmail.com>
2019-03-18 19:52:03 -0400

    Adding hidden files to watcher

--- app/watcher.py
+++ app/watcher.py
@@ -4,6 +4,7 @@ from watchdog.observers import Observer
 from .config import Config
 from .models import ImageModel
 
+import re
 import time
 import threading
 
@@ -18,32 +19,35 @@ class ImageFolderHandler(FileSystemEventHandler):
     def on_any_event(self, event):
 
         path = event.dest_path if event.event_type == "moved" else event.src_path
-        self._log(f'File {path} for {event.event_type}')
-        
-        # Check if thumbnails directory
-        folders = path.split('/')
-        i = folders.index("datasets")
-        if i+1 < len(folders) and folders[i+1] == "_thumbnails":
+                
+        if (
+            event.is_directory
+            # check if its a hidden file
+            or bool(re.search(r'\/\..*?\/', path))
+            or not path.lower().endswith(self.pattern)
+        ):
             return
         
-        if not event.is_directory and path.lower().endswith(self.pattern):
+        self._log(f'File {path} for {event.event_type}')
+        
+        folders = path.split('/')
+        image = ImageModel.objects(path=event.src_path).first()
 
-            image = ImageModel.objects(path=event.src_path).first()
+        if image is None and event.event_type != 'deleted':
+            self._log(f'Adding new file to database: {path}')
+            ImageModel.create_from_path(path).save()
 
-            if image is None and event.event_type != 'deleted':
-                self._log(f'Adding new file to database: {path}')
-                ImageModel.create_from_path(path).save()
+        elif event.event_type == 'moved':
+            self._log(f'Moving image from {event.src_path} to {path}')
+            image.update(path=path)
 
-            elif event.event_type == 'moved':
-                self._log(f'Moving image from {event.src_path} to {path}')
-                image.update(path=path)
-
-            elif event.event_type == 'deleted':
-                self._log(f'Deleting image from database {path}')
-                ImageModel.objects(path=path).delete()
+        elif event.event_type == 'deleted':
+            self._log(f'Deleting image from database {path}')
+            ImageModel.objects(path=path).delete()
 
     def _log(self, message):
         print(f'{self.PREFIX} {message}', flush=True)
+
 
 def run_watcher():
     observer = Observer()
