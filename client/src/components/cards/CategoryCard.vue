f66829c198633f4f45000b6b1ee23ee3a8d517e5
Matt DeBoer <matt.deboer@gmail.com>
2019-11-11 20:48:05 -0800

    cleanup comments and title

--- client/src/components/cards/CategoryCard.vue
+++ client/src/components/cards/CategoryCard.vue
@@ -50,7 +50,7 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title">Current Name: {{ category.name }}</h5>
+            <h5 class="modal-title">Category: {{ category.name }}</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -158,7 +158,7 @@ export default {
         .then(() => {
           this.axiosReqestSuccess(
             "Updating Category",
-            "Category name has been updated"
+            "Category successfully updated"
           );
           this.category.name = this.name;
           this.category.supercategory = this.supercategory;
