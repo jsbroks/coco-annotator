2568279fcb42d86748fae6b30ad8780befda0b56
chaimaa-louahabi <chaimaa.louahabi@gmail.com>
2021-08-31 14:03:36 +0000

    Change file name, change URI, Send image id only

---
+++ client/src/components/annotator/tools/DownloadSemanticSegButton.vue
@@ -0,0 +1,37 @@
+<script>
+import button from "@/mixins/toolBar/button";
+
+export default {
+  name: "DownloadSemanticSegButton",
+  mixins: [button],
+  props: {
+    image: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      name: "Download Semantic Segmentation Mask",
+      icon: "fa-image"
+    };
+  },
+  methods: {
+    download() {
+      let uri = "/api/image/semanticSegmentation/" + this.image.id + "?asAttachment=true";
+      //download URI
+      let link = document.createElement("a");
+      link.href = uri;
+      link.download = this.image.filename;
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    },
+    // Download function
+    execute() {
+      //this.$parent.save(this.download);
+      this.download();
+    }
+  }
+};
+</script>
